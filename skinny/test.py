Fix IOU calculation

skinny/test.py
@@ -39,7 +39,7 @@ def test(trained_model: Union[SkinnyBasic, SkinnyInception], parameters: Paramet
             logits, loss, masks = calculate_logits_and_loss(
                 images, masks, trained_model, parameters.criterion, parameters.device)
 
-            iou_sum += iou(logits, masks)
+            iou_sum += iou(logits[0][0], masks[0][0]).item()
             test_loss.append(loss)
 
         avg_test_loss = torch.stack(test_loss).mean()

skinny/train.py
@@ -59,7 +59,7 @@ def train(parameters: Parameters, train_dataset: Dataset, val_dataset: Dataset):
                         images, masks, model, parameters.criterion, parameters.device)
 
                     val_loss.append(loss)
-                    iou_score = iou(logits, masks)
+                    iou_score = iou(logits[0][0], masks[0][0])
                     bar.set_postfix(loss=loss.item(), iou=iou_score.item())
 
             avg_val_loss = torch.stack(val_loss).mean()