70ce642e15afb029a3161fdf3e5999a645a580b9
mmarcin5 <m.marcinkiewicz.5@stud.elka.pw.edu.pl>
2022-12-16 02:32:54 +0100

    Add missing argument in logits  and loss calculation

--- skinny/train.py
+++ skinny/train.py
@@ -9,7 +9,7 @@ import torch
 from tqdm.notebook import tqdm
 
 
-def train(parameters: Parameters, train_dataset: Dataset, val_dataset: Dataset) -> nn.Module:
+def train(parameters: Parameters, train_dataset: Dataset, val_dataset: Dataset):
     """
     Main training procedure.
     """
@@ -32,7 +32,7 @@ def train(parameters: Parameters, train_dataset: Dataset, val_dataset: Dataset) 
         for batch in bar:
             images, masks = batch
             logits, loss = calculate_logits_and_loss(
-                images, model, parameters.criterion)
+                images, masks, model, parameters.criterion)
 
             loss.backward()
             optimizer.step()
@@ -51,7 +51,7 @@ def train(parameters: Parameters, train_dataset: Dataset, val_dataset: Dataset) 
             for batch in val_loader:
                 images, masks = batch
                 logits, loss = calculate_logits_and_loss(
-                    images, model, parameters.criterion)
+                    images, masks, model, parameters.criterion)
                 val_loss.append(loss)
 
             avg_val_loss = torch.stack(val_loss).mean()
